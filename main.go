0e6a34f90c5bc45b6912183108c9335e36d8ce9f
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-10-07 20:14:42 +0200

    display share path

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ type LogEntry struct {
 	Rule             string
 	FileSize         string
 	FileLastModified string
-	FilePath         string
+	Path             string
 	Context          string
 }
 
@@ -39,15 +39,17 @@ type LogEntry struct {
 var embedfs embed.FS
 
 func main() {
-	file := flag.String("file", "", "snaffler output file in tsv format")
+	var file string
+	flag.StringVar(&file, "f", "", "snaffler output file in tsv format")
+	flag.StringVar(&file, "file", "", "snaffler output file in tsv format")
 	flag.Parse()
 
-	if *file == "" {
+	if file == "" {
 		flag.Usage()
 		return
 	}
 
-	content := strings.TrimSpace(string(must2(os.ReadFile(*file))))
+	content := strings.TrimSpace(string(must2(os.ReadFile(file))))
 	lines := strings.Split(content, "\r\n")
 	logEntries := make([]LogEntry, 0)
 
@@ -70,31 +72,35 @@ func main() {
 			Rule:             atIndex(values, 8),
 			FileSize:         atIndex(values, 9),
 			FileLastModified: atIndex(values, 10),
-			FilePath:         atIndex(values, 11),
+			Path:             atIndex(values, 11),
 			Context:          atIndex(values, 12),
 		}
 
-    if logEntry.Type == "[Info]" {
-      continue
-    }
+		if logEntry.Type == "[Info]" {
+			continue
+		}
+
+		if logEntry.Type == "[Share]" {
+			logEntry.Path = atIndex(values, 4)
+		}
 
 		logEntries = append(logEntries, logEntry)
 	}
 
-  tmpl := must2(template.ParseFS(embedfs, "html/*.html"))
+	tmpl := must2(template.ParseFS(embedfs, "html/*.html"))
 
-  http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-    res.Header().Add("Content-Type", "text/html")
-    tmpl.ExecuteTemplate(res, "index.html", logEntries)
-  })
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Add("Content-Type", "text/html")
+		tmpl.ExecuteTemplate(res, "index.html", logEntries)
+	})
 
-  http.HandleFunc("/style.css", func(res http.ResponseWriter, req *http.Request) {
-    res.Header().Add("Content-Type", "text/css")
-    file := must2(embedfs.Open("css/style.css"))
-    io.Copy(res, file)
-  })
+	http.HandleFunc("/style.css", func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Add("Content-Type", "text/css")
+		file := must2(embedfs.Open("css/style.css"))
+		io.Copy(res, file)
+	})
 
-  addr := ":8111"
-  fmt.Printf("[*] listen on %s\n", addr)
-  http.ListenAndServe(addr, nil)
+	addr := ":8111"
+	fmt.Printf("[*] listen on %s\n", addr)
+	http.ListenAndServe(addr, nil)
 }
